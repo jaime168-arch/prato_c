4657d4f58093d1f4e3a2b54b2d09df71158cd6cf
jaime_r_pereira <jaime_r_pereira@estudante.sesisenai.org.br>
2026-08-17 11:30:54 -0300

    uptade- Atualização do registro

--- public/editar_prato.php
+++ public/editar_prato.php
@@ -14,4 +14,22 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $preco = trim($_POST['preco']);
     $categoria = trim($_POST['categoria']);
     $usuario_id = trim($_POST['usuarios_id']);
+
+    if(!empty($nome) && !empty($descricao) && !empty($preco) && !empty($categoria) && !empty($usuario_id)) {
+        $stmt = $pdo->prepare("
+           UPDATE pratos
+           SET nome = :nome, descricao = :descricao, preco = :preco, categoria = :categoria, usuario_id = :usuario_id
+           WHERE id = :id
+        ");
+        $stmt->bindParam(':nome', $nome);
+        $stmt->bindParam(':descricao', $descricao);
+        $stmt->bindParam(':preco', $preco);
+        $stmt->bindParam(':categoria', $categoria);
+        $stmt->bindParam(':usuario_id', $usuario_id);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+
+        header("Location: index.php");
+        exit;
+    }
 }
