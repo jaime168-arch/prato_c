e872c33220ababacd9bfa2fe0ddfcaccf28db8ea
jaime_r_pereira <jaime_r_pereira@estudante.sesisenai.org.br>
2026-08-17 10:53:33 -0300

    update- Banco de dados com clientes e o pratos concluídos com sucesso

--- database/banco.sql
+++ database/banco.sql
@@ -6,3 +6,13 @@ CREATE TABLE IF NOT EXISTS clientes (
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE
 );
+
+CREATE TABLE IF NOT EXISTS pratos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    descricao TEXT NOT NULL,
+    preco DECIMAL (10,2) NOT NULL,
+    categoria VARCHAR(50) NOT NULL,
+    usuario_id INT NOT NULL,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
+);
