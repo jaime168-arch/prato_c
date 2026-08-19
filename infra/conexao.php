167e92b14a6487decda9f13b58ae4a19e2901886
jaime_r_pereira <jaime_r_pereira@estudante.sesisenai.org.br>
2026-08-19 10:58:06 -0300

    add- agr vai

--- infra/conexao.php
+++ infra/conexao.php
@@ -1,5 +1,4 @@
 <?php
-
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -7,15 +6,12 @@ error_reporting(E_ALL);
 $host = "localhost";
 $usuario = "root";
 $senha = "";
-
-$banco = "restaurante_ícaro"; 
+$banco = "restaurante_icaro"; 
 
 try {
-    
     $pdo = new PDO("mysql:host=$host;dbname=$banco;charset=utf8", $usuario, $senha);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    
     die("<div style='font-family: sans-serif; padding: 20px; background: #f8d7da; color: #721c24; border: 1px solid #f5c6cb; border-radius: 5px; margin: 20px;'>
             <strong>Erro ao conectar com o banco de dados:</strong><br>" . $e->getMessage() . "
          </div>");
