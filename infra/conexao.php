6c6b75e11cccd93e727322073deb1f5584e3c13b
jaime_r_pereira <jaime_r_pereira@estudante.sesisenai.org.br>
2026-08-19 10:12:51 -0300

    uptade- organização e teste de funcionamento do código

--- infra/conexao.php
+++ infra/conexao.php
@@ -8,6 +8,5 @@ try {
     $pdo = new PDO("mysql:host=$host;dbname=$banco;charset=utf8", $usuario, $senha);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Erro de conexão com o banco: " . $e->getMessage());
+    die("Erro ao conectar no banco: " . $e->getMessage());
 }
-?>
