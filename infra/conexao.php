bcb1f33a97d05d9e3d54a1f48e21d3479c281598
jaime_r_pereira <jaime_r_pereira@estudante.sesisenai.org.br>
2026-08-19 11:33:02 -0300

    add- finalmente ultimo teste

--- infra/conexao.php
+++ infra/conexao.php
@@ -1,18 +1,18 @@
 <?php
-// Força a exibição de qualquer erro de conexão na tela
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 $host = 'localhost';
-$dbname = 'restaurante_icaro'; // Nome exato que está no seu phpMyAdmin
+$dbname = 'restaurante_icaro';
 $usuario = 'root';
-$senha = ''; 
+$senha = '';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $usuario, $senha);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexão realizada com sucesso!";
 } catch (PDOException $e) {
-    // Se der erro no banco, vai escrever o motivo exato aqui!
     die("<h1>Erro de Conexão com o Banco:</h1> " . $e->getMessage());
 }
+?>
