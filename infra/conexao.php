9df06ccd9ec6c67d6735216b1d2d79d4f3c11e5b
jaime_r_pereira <jaime_r_pereira@estudante.sesisenai.org.br>
2026-08-19 11:05:03 -0300

    add- teste

--- infra/conexao.php
+++ infra/conexao.php
@@ -12,6 +12,7 @@ try {
     $pdo = new PDO("mysql:host=$host;dbname=$banco;charset=utf8", $usuario, $senha);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Erro na conexão com o banco de dados: " . $e->getMessage());
+    echo "<strong>Erro na conexão com o Banco de Dados:</strong> " . $e->getMessage();
+    exit;
 }
 ?>
